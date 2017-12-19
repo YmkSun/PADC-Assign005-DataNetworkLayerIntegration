9e67071c3fac974435d45145ccd8d4750dcb516e
Ye Myo Kyaw <ymksun90@gmail.com>
2017-12-19 21:50:20 +0630

    movie application before fragment and data helper fixed

--- app/src/main/java/com/androidapp/yemyokyaw/movieapp/data/persistance/MovieDBHelper.java
+++ app/src/main/java/com/androidapp/yemyokyaw/movieapp/data/persistance/MovieDBHelper.java
@@ -14,7 +14,22 @@ public class MovieDBHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "movie_data.db";
     private static final int DB_VERSION = 1;
-    private static final String SQL_CREATE_MOVIE_DATA_TABLE = "";
+    private static final String SQL_CREATE_MOVIE_DATA_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+            MovieEntry.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            MovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_POSTER_PATH + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_ORIGINAL_LANGUAGE + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_POPULARITY + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_VIDEO + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_ADULT + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_ORIGINAL_TITLE + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_VOTE_AVERAGE + " TEXT NOT NULL, " +
+            MovieEntry.COLUMN_VOTE_COUNT + " TEXT NOT NULL, " +
+            " UNIQUE (" + MovieEntry.COLUMN_TITLE + ") ON CONFLICT IGNORE" +
+            ");";
 
     public MovieDBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
