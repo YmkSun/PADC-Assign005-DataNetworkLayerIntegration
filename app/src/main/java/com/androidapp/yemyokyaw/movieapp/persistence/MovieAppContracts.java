52c83f56d1f40be6a8db9bd968f8d6f4c8318394
Ye Myo Kyaw <ymksun90@gmail.com>
2017-12-19 22:39:04 +0630

    DBHelper Fully Implemented

--- app/src/main/java/com/androidapp/yemyokyaw/movieapp/persistence/MovieAppContracts.java
+++ app/src/main/java/com/androidapp/yemyokyaw/movieapp/persistence/MovieAppContracts.java
@@ -1,8 +1,7 @@
-package com.androidapp.yemyokyaw.movieapp.data.persistance;
+package com.androidapp.yemyokyaw.movieapp.persistence;
 
 import android.content.ContentResolver;
 import android.content.ContentUris;
-import android.graphics.Movie;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -17,14 +16,17 @@ public class MovieAppContracts {
 
     public static final String CONTENT_AUTHORITY = MovieApp.class.getPackage().getName();
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
     public static final String PATH_MOVIE = "movie_data";
+    public static final String PATH_GENERE = "genere_data";
+    public static final String PATH_MOVIE_GENERE = "movie_genere_data";
 
     public static final class MovieEntry implements BaseColumns {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
         public static final String DIR_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
         public static final String ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
 
-        public static final String TABLE_NAME = "movie_data";
+        public static final String TABLE_NAME = PATH_MOVIE;
 
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_TITLE = "title";
@@ -43,13 +45,39 @@ public class MovieAppContracts {
         public static Uri buildMovieUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
+    }
 
-        public static Uri buildMovieUriWithTitle(String attractionTitle) {
-            return CONTENT_URI.buildUpon().appendQueryParameter(COLUMN_TITLE, attractionTitle).build();
+    public static final class GenereEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_GENERE).build();
+        public static final String DIR_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_GENERE;
+        public static final String ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_GENERE;
+
+        public static final String TABLE_NAME = PATH_GENERE;
+
+        public static final String COLUMN_GENERE_ID = "genere_id";
+        public static final String COLUMN_GENERE_NAME = "genere_name";
+
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class MovieGenereEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE_GENERE).build();
+        public static final String DIR_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE_GENERE;
+        public static final String ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE_GENERE;
+
+        public static final String TABLE_NAME = PATH_MOVIE_GENERE;
+
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_GENERE_ID = "genere_id";
+
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
         }
 
-        public static String getTitleFromParam(Uri uri) {
-            return uri.getQueryParameter(COLUMN_TITLE);
+        public static Uri buildMovieUriWithTitle(String attractionTitle) {
+            return CONTENT_URI.buildUpon().appendQueryParameter(COLUMN_GENERE_ID, attractionTitle).build();
         }
     }
 
---
+++ app/src/main/java/com/androidapp/yemyokyaw/movieapp/persistence/MovieDBHelper.java
@@ -0,0 +1,68 @@
+package com.androidapp.yemyokyaw.movieapp.persistence;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.androidapp.yemyokyaw.movieapp.persistence.MovieAppContracts.MovieEntry;
+import com.androidapp.yemyokyaw.movieapp.persistence.MovieAppContracts.GenereEntry;
+import com.androidapp.yemyokyaw.movieapp.persistence.MovieAppContracts.MovieGenereEntry;
+
+/**
+ * Created by yemyokyaw on 12/16/17.
+ */
+
+public class MovieDBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "movie_data.db";
+    private static final int DB_VERSION = 1;
+    private static final String SQL_CREATE_MOVIE_DATA_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+            MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            MovieEntry.COLUMN_ID + " INTEGER, " +
+            MovieEntry.COLUMN_TITLE + "  VARCHAR(256), " +
+            MovieEntry.COLUMN_BACKDROP_PATH + " TEXT, " +
+            MovieEntry.COLUMN_POSTER_PATH + " TEXT, " +
+            MovieEntry.COLUMN_RELEASE_DATE + " TEXT, " +
+            MovieEntry.COLUMN_ORIGINAL_LANGUAGE + " TEXT, " +
+            MovieEntry.COLUMN_OVERVIEW + " TEXT, " +
+            MovieEntry.COLUMN_POPULARITY + " REAL, " +
+            MovieEntry.COLUMN_VIDEO + " NUMERIC, " +
+            MovieEntry.COLUMN_ADULT + " NUMERIC, " +
+            MovieEntry.COLUMN_ORIGINAL_TITLE + " TEXT, " +
+            MovieEntry.COLUMN_VOTE_AVERAGE + " REAL, " +
+            MovieEntry.COLUMN_VOTE_COUNT + " INTEGER, " +
+            " UNIQUE (" + MovieEntry.COLUMN_TITLE + ") ON CONFLICT REPLACE" +
+            ");";
+
+    private static final String SQL_CREATE_GENERE_DATA_TABLE = "CREATE TABLE " + GenereEntry.TABLE_NAME + " (" +
+            GenereEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            GenereEntry.COLUMN_GENERE_ID + " INTEGER, " +
+            GenereEntry.COLUMN_GENERE_NAME + "  VARCHAR(256), " +
+            " UNIQUE (" + GenereEntry.COLUMN_GENERE_NAME + ") ON CONFLICT REPLACE" +
+            ");";
+
+    private static final String SQL_CREATE_MOVIE_GENERE_TABLE = "CREATE TABLE " + MovieGenereEntry.TABLE_NAME + " (" +
+            MovieGenereEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            MovieGenereEntry.COLUMN_MOVIE_ID + " INTEGER, " +
+            MovieGenereEntry.COLUMN_GENERE_ID + " INTEGER" +
+            ");";
+
+    public MovieDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_MOVIE_GENERE_TABLE);
+        db.execSQL(SQL_CREATE_GENERE_DATA_TABLE);
+        db.execSQL(SQL_CREATE_MOVIE_DATA_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + GenereEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + MovieGenereEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
